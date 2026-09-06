ff64432ee10df5241c35a5bc47cc82fbdb812275
trangtranoo6 <107522556+trangtranoo6@users.noreply.github.com>
2026-09-06 12:51:24 +0200

    Update posts.js

--- posts.js
+++ posts.js
@@ -31,7 +31,7 @@ const POSTS = [
         <img src="images/hongkong-cloud-doodles.jpg" alt="A bridge over the water at sunset, with two clouds doodled into cartoon animal faces">
       </figure>
       <p>These days, she has been blasting music while cycling around her neighborhood: Pak Shek Kok, Ma On Shan, and occasionally, when feeling adventurous, all the way to Sha Tin Wai. A whole new horizon has opened up. It's nothing like sitting on the MTR, a bus, or a tram and watching the city go by, and not quite the same as cycling to her highschool back in Nam Dinh either.
-      How to describe, it feels like freedom. Just her, her bike, and the road stretching along the coast. The music turns into a ribbon of sound with Doppler effect =)). Her brain switches into full NPC silly mode, just breathing in the scents of the plants and grass, mixed with the salty, fishy smell of the sea.
+      How to describe, it feels like freedom. Just her, her bike, and the road stretching along the coast. The music turns into a ribbon of sound with Doppler effect =)). Her brain switches into full NPC silly mode, just breathing in the scents of plants and grass, mixed with the salty, fishy smell of the sea.
       Now she finally understands why they call this place 香港（Fragrant Harbour).</p>
       <p>Her friends always say Hong Kong is boring, but somehow she feels the opposite. Perhaps it suits someone as bland as her. Bland people find each other, she guesses =)).</p>
       There's something about Hong Kong that is modern yet traditional, artificial yet natural. There's the hustle of Central, but also the slow, unhurried weekends spent hiking and staring at the sky. Skyscrapers stand right next to mountains, and bays sit right alongside highways.
